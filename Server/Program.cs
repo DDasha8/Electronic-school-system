3935f9fa369c3a5428f5cd579e27848ee1ddec89
Konstantin <65172983+badboyinahood@users.noreply.github.com>
2021-08-04 18:35:13 +0300

    Update Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -12,34 +12,27 @@ namespace Server
         {
             // получаем адреса для запуска сокета
             IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
-
             // создаем сокет
             Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try
             {
                 // связываем сокет с локальной точкой
                 listenSocket.Bind(ipPoint);
-
                 listenSocket.Listen(10);
-
                 Console.WriteLine("Сервер запущен. Ожидание подключения...");
-
                 while (true)
                 {
                     Socket handler = listenSocket.Accept();
                     StringBuilder builder = new StringBuilder();
-                    int bytes = 0; 
-                    byte[] data = new byte[256]; 
-
+                    int bytes = 0;
+                    byte[] data = new byte[256];
                     do
                     {
                         bytes = handler.Receive(data);
                         builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
                     }
                     while (handler.Available > 0);
-
                     Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
-
                     // отправляем ответ
                     string message = "ваше сообщение доставлено";
                     data = Encoding.Unicode.GetBytes(message);
